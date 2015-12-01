243acb76b876ce4cf6e6da61d4e9147711cc36e3
Kritsanasak Kuntaros <roboconk@gmail.com>
2015-12-01 16:25:51 +0700

    Remove other theme size

--- wp-content/themes/salient-child/functions.php
+++ wp-content/themes/salient-child/functions.php
@@ -5,4 +5,13 @@ function salient_child_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 
-?>
+/**
+ * Override old function by remove other image size
+ */
+if (!function_exists('nectar_add_image_sizes')) {
+	function nectar_add_image_sizes(){
+		add_theme_support( 'post-thumbnails' );
+	}
+}
+
+nectar_add_image_sizes();
