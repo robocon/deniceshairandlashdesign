a6e8ceeaf518c7ff4e605eecc5380b3974c630c7
Kritsanasak Kuntaros <roboconk@gmail.com>
2015-12-04 18:22:41 +0700

    - update Change to custom html

--- wp-content/themes/salient-child/js/coupon_print.js
+++ wp-content/themes/salient-child/js/coupon_print.js
@@ -3,27 +3,32 @@ jQuery.noConflict();
   $(function() {
     // Your Script
 
-    if( $('.print-coupon-contain').length > 0 ){
-    	$(document).on('click', '.print-coupon-contain .nectar-button', function(e){
-	    	e.preventDefault();
+    if( $('.btn-print').length > 0 ){
 
-	    	var test_txt = '<style type="text/css">';
-	    	test_txt += 'body{ text-align: center; color: #222222; font-family: Arial; }';
-	    	test_txt += 'h3{ color: #ef1616; font-weight: bold; font-family: Lovelo; }';
-	    	test_txt += '.coupon-custom{ width: 300px; }';
-			test_txt += '.nectar-button{ color: #27ccc0; font-weight: bold; text-decoration: none; font-family: Lovelo; }';
-	    	test_txt += 'p:last-child{ color: #ada9a9; }';
-	    	test_txt += '</style>';
-	    	test_txt += '<div class="coupon-custom">';
-	    	test_txt += $(this).parents('.pricing-column').html();
-	    	test_txt += '</div>';
+		$(document).on('click', '.btn-print', function(e){
+			e.preventDefault();
 
-	    	var testprint = window.open('','test print','height=300,width=300');
-		    testprint.document.body.innerHTML = test_txt;
-		    testprint.print();
-		    testprint.close();
+			var txt = '<style type="text/css">';
+			txt += '.coupon-contain {padding: 1em 0;color: #222222;line-height: 1.2em;width: 100%;text-align: center;float: left;border: 1px solid #dddddd;font-family: Arial;font-size: 14px;}';
+			txt += '.coupon-contain .header {color: #ef1616;font-weight: bold;font-size: 20px;}';
+			txt += '.coupon-contain .price {margin: 0.5em 0;font-family: Lovelo;}';
+			txt += '.coupon-contain .price s {color: #ef1616;width: 220px;}';
+			txt += '.coupon-contain .price s span {color: #222222;}';
+			txt += '.coupon-contain .btn-print {color: #27ccc0;font-weight: bold;cursor: pointer;margin: 1em 0;font-family: Lovelo;}';
+			txt += '.coupon-contain .bottom {color: #ada9a9;}';
+			txt += '</style>';
+			txt += '<div class="coupon-contain">';
+			txt += $(this).parent('.coupon-contain').html();
+			txt += '</div>';
 
-	    });
+
+			var wcoupon = window.open('','test print','height=300,width=300');
+			wcoupon.document.body.innerHTML = txt;
+			wcoupon.print();
+			wcoupon.close();
+		});
+
+
     }
 	
   });
